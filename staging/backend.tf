8fb5d15f73ddd04a0c640ee31f648e4b0aa3a56c
stephentyers1975 <steve.tyers@gmail.com>
2025-06-01 16:36:09 +0100

    changed provider for staging

--- staging/backend.tf
+++ staging/backend.tf
@@ -1,12 +1,31 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstagebackend2024piyush"
-    container_name      = "tfstate"
-    key                 = "stage.tfstate"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.12.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~> 3.0.2"
+    }
+     tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0" # Use a compatible version
+    }
   }
+
+  required_version = ">= 1.9.0"
 }
+# configures the provider
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+}
+provider "azuread" {
+  
 }
