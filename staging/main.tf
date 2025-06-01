fe1a728e46310b2bfd2af90517e742d93ab09ed9
stephentyers1975 <steve.tyers@gmail.com>
2025-06-01 12:35:07 +0100

    edited staging role assignments

--- staging/main.tf
+++ staging/main.tf
@@ -23,6 +23,17 @@ resource "azurerm_role_assignment" "rolespn" {
   ]
 }
 
+resource "azurerm_role_assignment" "kv_admin" {
+
+  scope                = "/subscriptions/${var.SUB_ID}"
+  role_definition_name = "Key Vault Contributor"
+  principal_id         = module.ServicePrincipal.service_principal_object_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
+
 module "keyvault" {
   source                      = "../modules/keyvault"
   keyvault_name               = var.keyvault_name
@@ -33,7 +44,7 @@ module "keyvault" {
   service_principal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
 
   depends_on = [
-    module.ServicePrincipal
+    azurerm_role_assignment.kv_admin
   ]
 }
 
